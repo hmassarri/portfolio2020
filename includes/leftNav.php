473d21453fce0b6ae2db7f38bedfa3d9d4067981
Hugo Massarri <hmassarri@PRNNVENKATA.alpha.eduh.co>
2020-01-22 09:00:03 -0500

    Moved nav to php, fixing card layout, added nav to portfolio

---
+++ includes/leftNav.php
@@ -0,0 +1,10 @@
+<!-- Nav -->
+<nav id="nav">
+  <ul>
+    <li><a href="index.php" id="top-link" class="active active-locked"><span class="icon solid fa-home">Intro</span></a></li>
+    <li><a href="portfolio-home.html" id="portfolio-link"><span class="icon solid fa-th">Portfolio</span></a></li>
+    <li><a href="process.html" id="process-link"><span class="icon solid fa-cogs">Process</span></a></li>
+    <li><a href="aboutme.html" id="about-link"><span class="icon solid fa-user">About Me</span></a></li>
+    <li><a href="contact.html" id="contact-link"><span class="icon solid fa-envelope">Contact</span></a></li>
+  </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE HTML>
+<!--
+	Prologue by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+
+<head>
+	<title>Hugo M. / Portfolio 2020</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+</head>
+
+<body class="is-preload">
+
+	<!-- Header -->
+	<div id="header">
+
+		<div class="top">
+
+			<!-- Logo -->
+			<div id="logo">
+				<span class="image avatar48"><img src="images/checkerboard-pink.png" alt="" /></span>
+				<h1 id="title">Hugo Massarri</h1>
+				<p>UIX Designer + Developer</p>
+			</div>
+
+			<!-- Nav -->
+			<?php include 'includes/leftNav.php'; ?>
+			
+
+		</div>
+
+		<div class="bottom">
+
+			<!-- Social Icons -->
+			<ul class="icons">
+				<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+				<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+				<li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+				<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
+				<li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+			</ul>
+
+		</div>
+
+	</div>
+
+	<!-- Main -->
+	<div id="main">
+
+		<!-- Intro -->
+		<section id="top" class="">
+			<div class="container">
+
+				<header>
+
+					<h2 class="alt">Hi!<br />I'm <strong>the Index Page of</strong> this site, which is a  responsive<br />
+					site designed by <a href="http://html5up.net">Hugo M.</a>.</h2>
+					<p><img src="images/avatar1.jpg" class="image" /></p>
+
+				</header>
+
+				<footer>
+					<!-- <a href="#portfolio" class="button scrolly">Go straight to my work</a> -->
+				</footer>
+
+			</div>
+		</section>
+
+	</div>
+
+	<!-- Footer -->
+	<div id="footer">
+
+		<!-- Copyright -->
+		<ul class="copyright">
+			<li>&copy; Copyright &copy; 1999-2020 Hugo M. / hugomassarri.com. All rights reserved.</li>
+			<li></li>
+		</ul>
+
+	</div>
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.scrolly.min.js"></script>
+	<script src="assets/js/jquery.scrollex.min.js"></script>
+	<script src="assets/js/browser.min.js"></script>
+	<script src="assets/js/breakpoints.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
