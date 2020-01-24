77ab2056fe632a4ca6e91d5c5b116a2db51c7d7a
hmassarri <hugo@hugomasarri.com>
2020-01-24 09:24:00 -0500

    fixed php for portfolio

--- includes/leftNav.php
+++ includes/leftNav.php
@@ -6,9 +6,12 @@
   $currentPage = pageName();
   //echo $currentPage;
 
-  $url = ($_SERVER["SCRIPT_NAME"]);
-  $items = explode('/', $url);
-  $portfolioFolder = $items[sizeof($items) - 2];
+  function folderName() {
+    $url = ($_SERVER["SCRIPT_NAME"]);
+    $items = explode('/', $url);
+    return $items[sizeof($items) - 2];
+  }
+  $folderName = folderName();
 
 ?>
 
@@ -27,11 +30,11 @@
     <!-- Nav -->
     <nav id="nav">
       <ul>
-        <li><a href="<?php echo $root . 'index.php' ?>" id="top-link" class="<?php echo $currentPage == 'index.php' ? 'active active-locked':NULL ?>"><span class="icon solid fa-home">Intro</span></a></li>
-        <li><a href="<?php echo $root . 'portfolio-home.html' ?>" id="portfolio-link" class="<?php echo $currentPage == 'portfolio-home.html' || $portfolioFolder ? 'active active-locked':NULL ?>"><span class="icon solid fa-th">Portfolio</span></a></li>
-        <li><a href="<?php echo $root . 'process.html' ?>" id="process-link" class="<?php echo $currentPage == 'process.html' ? 'active active-locked':NULL ?>"><span class="icon solid fa-cogs">Process</span></a></li>
-        <li><a href="<?php echo $root . 'aboutme.html' ?>" id="about-link" class="<?php echo $currentPage == 'aboutme.html' ? 'active active-locked':NULL ?>"><span class="icon solid fa-user">About Me</span></a></li>
-        <li><a href="<?php echo $root . 'contact.html' ?>" id="contact-link" class="<?php echo $currentPage == 'contact.html' ? 'active active-locked':NULL ?>"><span class="icon solid fa-envelope">Contact</span></a></li>
+        <li><a href="<?php echo $root . 'index.php' ?>" id="top-link" class="<?php echo $currentPage == 'index.php' ? 'active':NULL ?>"><span class="icon solid fa-home">Intro</span></a></li>
+        <li><a href="<?php echo $root . 'portfolio-home.html' ?>" id="portfolio-link" class="<?php echo $folderName == 'portfolio' ? 'active':NULL ?>"><span class="icon solid fa-th">Portfolio</span></a></li>
+        <li><a href="<?php echo $root . 'process.html' ?>" id="process-link" class="<?php echo $currentPage == 'process.html' ? 'active':NULL ?>"><span class="icon solid fa-cogs">Process</span></a></li>
+        <li><a href="<?php echo $root . 'aboutme.html' ?>" id="about-link" class="<?php echo $currentPage == 'aboutme.html' ? 'active':NULL ?>"><span class="icon solid fa-user">About Me</span></a></li>
+        <li><a href="<?php echo $root . 'contact.html' ?>" id="contact-link" class="<?php echo $currentPage == 'contact.html' ? 'active':NULL ?>"><span class="icon solid fa-envelope">Contact</span></a></li>
       </ul>
     </nav>
 
@@ -44,6 +47,7 @@
       <li><a href="#" class="icon brands fa-instagram"><span class="label">Twitter</span></a></li>
       <li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
       <li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+      <?php echo $folderName; ?>
     </ul>
 
   </div>
